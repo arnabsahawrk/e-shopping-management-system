4c46d727f3bf3b947d2c744b0680f99bde259f6a
arnabw <arnabsahawrk@gmail.com>
2025-10-22 22:17:35 +0600

    list

--- list.py
+++ list.py
@@ -1,2 +1,34 @@
 class List:
-    pass
+    def __init__(self) -> None:
+        self.products = []
+
+    def add_product(self, product):
+        self.products.append(product)
+
+    def find_product(self, product_name):
+        for product in self.products:
+            if product.name.lower() == product_name.lower():
+                return product
+        return None
+
+    def remove_product(self, product_name):
+        product = self.find_product(product_name)
+
+        if product:
+            self.products.remove(product)
+            print("product deleted.")
+        else:
+            print("product no found.")
+
+    def view_products(self):
+        if len(self.products):
+            print("\t**********************")
+            print("\tALL LISTED PRODUCTS")
+            print("\t**********************")
+            for product in self.products:
+                print(
+                    f"Name: {product.name}\tPrice: {product.price}\t",
+                    f"Quantity: {product.quantity}",
+                )
+        else:
+            print("No listed products available yet.")
